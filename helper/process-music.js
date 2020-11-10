faf642d34e915520fbd215265845b24f707d01ea
Tony Huỳnh <mr.tonyhuynh@gmail.com>
2020-11-11 01:23:52 +0700

    Update

---
+++ helper/process-music.js
@@ -0,0 +1,139 @@
+/***********************\
+ *      IMPORT
+ \***********************/
+
+const fs = require('fs')
+const path = require('path')
+const parse = require('csv-parse')
+const transform = require('stream-transform')
+const stringify = require('csv-stringify')
+
+const _ = require('lodash')
+
+module.exports = function process(fileObj, options) {
+    // OPTIONS
+    const {
+        DATASET_DIR_NAME,
+        OUTPUT_DIR_NAME,
+        EXCLUDE_ROLES,
+        EXCLUDE_NAMES
+    } = options;
+
+    // PATHS
+    const inputFilePath = path.resolve(DATASET_DIR_NAME, fileObj.fileName);
+    const outputFilePath = path.resolve(OUTPUT_DIR_NAME, fileObj.fileName);
+
+    // OUTPUT
+    let output = []
+
+    // PARSER
+    const parser = parse({
+        bom: true,
+        relax: true,
+        columns: true,
+        delimiter: ',',
+        ltrim: true,
+        rtrim: true,
+        // to_line: 30,
+        on_record: (record) => _.pick(record, fileObj.cols)
+    });
+
+
+        // .pipe(fs.createWriteStream(outputFilePath));
+
+    const exportResult = function(result){
+        result = _.map(result, o => {
+           return _.map(o, i => _.trim(i));
+        });
+
+        // STRINGIFIER
+        const stringifier = stringify(result, {
+            header: true,
+            columns: {
+                role: 'Role',
+                eng: 'Eng',
+                chi: 'Chi'
+            }
+        }, function(err, output){
+            if (err) throw err;
+            fs.writeFile(outputFilePath, output, (err) => {
+                if (err) throw err;
+                console.log(`${outputFilePath} saved.`);
+            });
+        });
+    }
+
+    // parser.on('data', function (row) {
+        // console.log(row);
+        // for (const col of fileObj.cols) {
+        //     let parts = _.split(row[col], ';'); // Tách bởi dấu ;
+        //     parts = _.compact(parts); // Loại bỏ các giá trị false, 0, '' ...
+        //     parts = _.map(parts, part => {
+        //         let [key, val] = _.split(part, /[：:︰﹕]/, 2);
+        //         key = _.trim(_.toLower(key));
+        //         val = _.replace(val, /\n/g, " "); // Remove line-break
+        //         // val = split(val, ',').map(o => trim(o));
+        //         return [key, val];
+        //     }); // Tách [role,[names]]
+        //     parts = _.filter(parts, part => !EXCLUDE_ROLES.includes(part[0])); // Loại bỏ các roles không hợp lệ
+        //     parts = _.map(parts, part => part[1]); // Chỉ lấy phần name, bỏ phần role
+        //     parts = _.flattenDeep(parts); // Làm phẳng array
+        //     output = _.concat(output, parts); // Merge vào output
+        // }
+    // });
+    //
+    // parser.on('end', async function () {
+    //     output = _.compact(output); // Loại bỏ các giá trị trống
+    //     output = _.filter(output, o => !EXCLUDE_NAMES.includes(o)); // Loại bỏ các tên không hợp lệ
+    //     output = _.uniq(output); // Loại bỏ các giá trị trùng lặp
+    //     output = _.uniq(output); // Loại bỏ các giá trị trùng lặp
+    //     console.log(`${fileObj.fileName} result: %d`, output.length);
+    //     for (let i = 0; i < output.length; i++) {
+    //         stringifier.write([_.trim(output[i])]);
+    //     }
+    //     stringifier.end();
+    // });
+
+    // EXTRACT DATA FROM STRING
+    const extractString = transform(function(data){
+        data = _.mapValues(data, function(o) {
+            let parts = _.split(o, ';'); // Split by ;
+            parts = _.compact(parts); // Remove false, 0, '' ...
+            parts = _.map(parts, p => _.split(p, /[：:︰﹕]/, 2));
+            return parts;
+        });
+
+        const eng = data[fileObj.cols[0]];
+        const chi = data[fileObj.cols[1]];
+
+        const output = [];
+
+        for(let i = 0; i < _.size(eng); i++){
+            output.push(_.concat(eng[i],_.tail(chi[i])));
+        }
+        // console.log(output);
+        return output;
+    });
+
+    // FILTER ROLES
+    const filterRoles = transform(function(data){
+        data = _.filter(data, function(o) {
+            return !_.includes(EXCLUDE_ROLES, _.toLower(_.trim(_.head(o))));
+        });
+        // console.log(data);
+        return data;
+    }).on('data', function(row){
+        output = _.concat(output, row);
+    }).on('end', function() {
+        // console.log(output);
+        exportResult(output);
+    });
+
+    // READER STREAM
+    const readerStream = fs.createReadStream(inputFilePath);
+    readerStream
+        .pipe(parser)
+        .pipe(extractString)
+        .pipe(filterRoles);
+        // .pipe(fs.createWriteStream(outputFilePath));
+}
--- index.js
+++ index.js
@@ -1,22 +1,8 @@
 /***********************\
  *      IMPORT
 \***********************/
-
-const fs = require('fs')
-const path = require('path')
-const parse = require('csv-parse')
-const stringify = require('csv-stringify')
-const pick = require('lodash/pick')
-const split = require('lodash/split')
-const compact = require('lodash/compact')
-const map = require('lodash/map')
-const filter = require('lodash/filter')
-const uniq = require('lodash/uniq')
-const trim = require('lodash/trim')
-const toLower = require('lodash/toLower')
-const flattenDeep = require('lodash/flattenDeep')
-const concat = require('lodash/concat')
-const replace = require('lodash/replace')
+const _ = require('lodash')
+const processMusic = require('./helper/process-music')
 
 /***********************\
  *      VARIABLES
@@ -24,12 +10,17 @@ const replace = require('lodash/replace')
 
 const DATASET_DIR_NAME = 'datasets'
 const OUTPUT_DIR_NAME = 'data-result'
-const EXCLUDE_ROLES = ['主辦', 'Presenter', '演出團體 Performing Group'].map(o => trim(toLower(o)))
+const EXCLUDE_ROLES = ['Presenter', 'Performing Group'].map(o => _.trim(_.toLower(o)))
 const EXCLUDE_NAMES = ['nil'];
 
 const musicFile = {
     fileName: "music.csv",
-    cols: ['Presenter_Eng', 'Presenter_Chi', 'Work_Eng', 'Work_Chi'],
+    cols: [
+        'Presenter_Eng',
+        'Presenter_Chi',
+        // 'Work_Eng',
+        // 'Work_Chi'
+    ],
 }
 const danceFile = {
     fileName: "dance.csv",
@@ -55,77 +46,12 @@ const exampleFile = {
 }
 
 /***********************\
- *      FUNCTIONS
-\***********************/
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function process(fileObj) {
-    const inputFilePath = path.resolve(DATASET_DIR_NAME, fileObj.fileName);
-    const outputFilePath = path.resolve(OUTPUT_DIR_NAME, fileObj.fileName);
-    const readerStream = fs.createReadStream(inputFilePath);
-    readerStream.setEncoding('UTF8'); // Set the encoding to be utf8.
-
-    let output = []
-
-    const stringifier = stringify({
-        quote: false
-    });
-
-    stringifier.pipe(fs.createWriteStream(outputFilePath))
-
-    const parser = parse({
-        bom: true,
-        relax: true,
-        columns: true,
-        delimiter: ',',
-        ltrim: true,
-        rtrim: true,
-        on_record: (record) => pick(record, fileObj.cols)
-    });
-
-    // Use the readable stream api
-    parser
-        .on('data', function (row) {
-            for (const col of fileObj.cols) {
-                let parts = split(row[col], ';'); // Tách bởi dấu ;
-                parts = compact(parts); // Loại bỏ các giá trị false, 0, '' ...
-                parts = map(parts, part => {
-                    let [key, val] = split(part, /[：:︰﹕]/, 2);
-                    key = trim(toLower(key));
-                    val = replace(val, /\n/g, " "); // Remove line-break
-                    // val = split(val, ',').map(o => trim(o));
-                    return [key, val];
-                }); // Tách [role,[names]]
-                parts = filter(parts, part => !EXCLUDE_ROLES.includes(part[0])); // Loại bỏ các roles không hợp lệ
-                parts = map(parts, part => part[1]); // Chỉ lấy phần name, bỏ phần role
-                parts = flattenDeep(parts); // Làm phẳng array
-                output = concat(output, parts); // Merge vào output
-            }
-        }).on('end', async function () {
-            output = compact(output); // Loại bỏ các giá trị trống
-            output = filter(output, o => !EXCLUDE_NAMES.includes(o)); // Loại bỏ các tên không hợp lệ
-            output = uniq(output); // Loại bỏ các giá trị trùng lặp
-
-
-            await sleep(1000);
-            output = uniq(output); // Loại bỏ các giá trị trùng lặp
-            await sleep(1000);
-            console.log(`${fileObj.fileName} result: %d`, output.length);
-            for (let i = 0; i < output.length; i++) {
-                stringifier.write([trim(output[i])]);
-            }
-            stringifier.end();
-        });
-
-    readerStream.pipe(parser);
-}
-
-/***********************\
  *      MAIN APP
 \***********************/
 
-// process(exampleFile);
-process(musicFile);
+processMusic(musicFile, {
+    DATASET_DIR_NAME,
+    OUTPUT_DIR_NAME,
+    EXCLUDE_ROLES,
+    EXCLUDE_NAMES
+});
